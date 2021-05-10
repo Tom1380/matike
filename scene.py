29385ba910b917fffcc29f90384ad55bd5ba181a
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2021-05-10 16:38:00 +0200

    Removed left-over example scenes.

--- scene.py
+++ scene.py
@@ -1,53 +1,6 @@
 from manim import *
 
 
-class SquareToCircle(Scene):
-    def construct(self):
-        circle = Circle()                    # create a circle
-        circle.set_fill(BLUE, opacity=0.5)   # set color and transparency
-
-        square = Square()                    # create a square
-        square.set_fill(RED, opacity=0.5)   # set color and transparency
-        square.flip(RIGHT)                   # flip horizontally
-        square.rotate(-3 * TAU / 8)          # rotate a certain amount
-
-        self.play(Create(square))      # animate the creation of the square
-        # interpolate the square into the circle
-        self.play(Transform(square, circle))
-        self.play(FadeOut(square))           # fade out animation
-
-
-class Shapes(Scene):
-    def construct(self):
-        circle = Circle()
-        square = Square()
-        triangle = Triangle()
-
-        circle.shift(LEFT)
-        square.shift(UP)
-        triangle.shift(RIGHT)
-
-        self.add(circle, square, triangle)
-        self.wait(1)
-
-
-class MobjectPlacement(Scene):
-    def construct(self):
-        circle = Circle()
-        square = Square()
-        triangle = Triangle()
-
-        # place the circle two units left from the origin
-        circle.move_to(LEFT * 2)
-        # place the square to the left of the circle
-        square.next_to(circle, LEFT)
-        # align the left border of the triangle to the left border of the circle
-        triangle.align_to(circle, LEFT)
-
-        self.add(circle, square, triangle)
-        self.wait(1)
-
-
 def get_internal_circumferences():
     return [Circle(radius=(1 - i / 20), color=TEAL).set_stroke(width=3) for i in range(1, 21)]
 
